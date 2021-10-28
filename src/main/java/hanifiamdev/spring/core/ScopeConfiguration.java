2a662ee659e990ece68a54af1305ca0b554eb3b2
hanifiamdev <hanifamrullahdev@gmail.com>
2021-10-29 03:39:08 +0700

    Scope

--- src/main/java/hanifiamdev/spring/core/ScopeConfiguration.java
+++ src/main/java/hanifiamdev/spring/core/ScopeConfiguration.java
@@ -7,11 +7,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
 /*
-* Scope
+* # Scope #
 * Scope merupakan strategy cara sebuah object dibuat
 * Secara default strategy object di Spring adalah singleton, artinya hanya dibuat sekali, dan ketika kita akses, akan mengembalikan object yang sama
 * Namun kita juga bisa mengubah scope bean yang kita mau di spring
 * Untuk mengubah scope sebuah bean, kita bisa ditambahkan annotation @Scope(value="namaScope")
+* # Jenis Bean Scope #
+*   - singleton -> Hanya dibuat sekali dalam dalam Spring IoC ( ini defaultnya )
+*   - prototype -> Selalu dibuat object baru setiap kali bean diakses
+*   - request -> Dibuat baru per HTTP Request( hanya untuk Web App )
+*   - session -> Dibuat baru per HTTP Session ( hanya untuk Web App )
+*   - application -> Dibuat baru per ServletContext ( hanya untuk Web App )
+*   - websocket -> Dibuat baru per WebSocket ( hanya untuk Websocket App )
 *
 * */
 
