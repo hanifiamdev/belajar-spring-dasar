818cd8395decb2b7c6ffacf443f13753fafc43c3
hanifiamdev <hanifamrullahdev@gmail.com>
2021-10-31 22:38:10 +0700

    Life Cycle

---
+++ src/main/java/hanifiamdev/spring/core/LifeCycleConfiguration.java
@@ -0,0 +1,28 @@
+package hanifiamdev.spring.core;
+
+import hanifiamdev.spring.core.data.Connection;
+import org.springframework.context.annotation.Bean;
+
+
+/*
+* Life Cycle
+*
+* Spring Container memiliki alur hidup, dan jika ingin berinteraksi dengan alur hidup nya spring. ini juga sangat mungkin bisa dilakukan
+* Saat pertama kali spring berjalan dan sudah selesai membuat bean, Spring akan memberitahu bean tersebut bahwa bean tersebut sudah siap, artinya semua dependencies sudah dimasukkan\
+* Dan ketika aplikasi spring mati, Spring juga akan memberitahu semua bean bahwa bean tersebut akan dihancurkan
+*
+* Life Cycle Callback
+* Secara default, bean tidak bisa tahu alur hidup spring ketika selesai membuat bean dan ketika akan menghancurkan bean
+* agar dapat berinteraksi dengan alur hidup spring terjadi, amak kita bisa implements interface initializingBean dan DisposableBean
+* https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/beans/factory/InitializingBean.html
+* DisposableBean diguakan jika kita ingin bereaksi ketika spring akan  menghancurkan bean
+* https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/beans/factory/DisposableBean.html
+* */
+public class LifeCycleConfiguration {
+
+    @Bean
+    public Connection connection() {
+        return new Connection();
+    }
+
+}
---
+++ src/test/java/hanifiamdev/spring/core/LifeCycleTest.java
@@ -0,0 +1,30 @@
+package hanifiamdev.spring.core;
+
+import hanifiamdev.spring.core.data.Connection;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class LifeCycleTest {
+    // memakai kelas turunan ApplicationContext supaya bisa menjalankan implement dari destroy
+    private ConfigurableApplicationContext applicationContext;
+
+    @BeforeEach
+    void setUp() {
+        applicationContext = new AnnotationConfigApplicationContext(LifeCycleConfiguration.class);
+        // applicationContext.registerShutdownHook(); // ini sama untuk mematikan apliaksi
+    }
+
+    @AfterEach
+    void tearDown() {
+        applicationContext.close();
+    }
+
+    @Test
+    void testConnection() {
+        Connection connection = applicationContext.getBean(Connection.class);
+    }
+}
