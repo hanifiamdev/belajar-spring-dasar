b1e9963942b8268f387b0fb024104d1601bf9e81
hanifiamdev <hanifamrullahdev@gmail.com>
2021-11-03 06:23:38 +0700

    Startup Failure

--- src/main/java/hanifiamdev/spring/core/application/FooApplication.java
+++ src/main/java/hanifiamdev/spring/core/application/FooApplication.java
@@ -1,5 +1,6 @@
 package hanifiamdev.spring.core.application;
 
+import hanifiamdev.spring.core.data.Bar;
 import hanifiamdev.spring.core.data.Foo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -30,8 +31,21 @@ public class FooApplication {
         System.out.println(foo);
     }
 
+    /*@Bean
+    public Foo foo() { // buang parameter bar klw
+        return new Foo();
+    }*/
+
+    /*
+    * ## Startup Failure ##
+    * Salah satu yang menarik di Spring Boot adalah, Spring Boot memiliki fitur FailureAnalyzer
+    * FailureAnalyzer digunakan untuk melakukan analisa ketika terjadi eror startup yang menyebabkan apliaksi tidak mau berjalan
+    * Spring sudah menyediakan banyak sekali class implementtasi FailureAnalyzer yang bisa mempermudah kita menganalisa kesalahan ketika terjadi startup failure
+    * https://docs.spring.io/spring-boot/docs/current/api/org/springframework/boot/diagnostics/FailureAnalyzer.html
+    *
+    * */
     @Bean
-    public Foo foo() {
+    public Foo foo(Bar bar) { // pakai method ini untuk test starup failure
         return new Foo();
     }
 }
---
+++ src/test/java/hanifiamdev/spring/core/application/WithoutSpringBootTest.java
@@ -0,0 +1,24 @@
+package hanifiamdev.spring.core.application;
+
+import hanifiamdev.spring.core.data.Foo;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class WithoutSpringBootTest {
+
+    private ConfigurableApplicationContext applicationContext;
+
+    @BeforeEach
+    void setUp() {
+        applicationContext = new AnnotationConfigApplicationContext(FooApplication.class);
+        applicationContext.registerShutdownHook();
+
+    }
+
+    @Test
+    void testErorFoo () {
+        Foo foo = applicationContext.getBean(Foo.class);
+    }
+}
